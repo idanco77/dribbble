9c9824db769af446960329de55618c27c74ec14b
idanc <idanc@smarti.co.il>
2020-04-05 11:01:00 +0300

    m

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -20,7 +20,7 @@ class VerificationController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('signed')->only('verify');
+       // $this->middleware('signed')->only('verify');
         $this->middleware('throttle:6,1')->only('verify', 'resend');
     }
 
@@ -44,6 +44,24 @@ class VerificationController extends Controller
 
     public function resend(Request $request)
     {
+        $this->validate($request, [
+            'email' => ['email', 'required']
+        ]);
 
+        $user = User::where('email', $request->email)->first();
+        if(! $user) {
+            return response()->json(['errors' => [
+                'email' => 'No user could be found'
+            ], 422]);
+        }
+
+        if ($user->hasVerifiedEmail()) {
+            return response()->json(['errors' => [
+                'message' => 'Email address has verified']], 422);
+        }
+
+        $user->sendEmailVerificationNotification();
+
+        return response()->json(['suatus' => 'verification link sent']);
     }
 }
--- app/models/User.php
+++ app/models/User.php
@@ -29,6 +29,11 @@ class User extends Authenticatable implements JWTSubject, MustVerifyEmail
         'location'
     ];
 
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new \App\Notifications\VerifyEmail());
+    }
+
     public function getJWTIdentifier()
     {
         return $this->getKey();

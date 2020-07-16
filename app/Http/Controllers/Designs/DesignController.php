4fe5f94d79584c4f0f52ae0021136b2df780d7c4
idanc <idanc@smarti.co.il>
2020-07-16 14:43:01 +0300

    m

--- app/Http/Controllers/Designs/DesignController.php
+++ app/Http/Controllers/Designs/DesignController.php
@@ -10,7 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 
-class DesignController extends Controller
+class  DesignController extends Controller
 {
     protected $designs;
 
